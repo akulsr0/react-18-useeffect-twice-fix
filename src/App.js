6bd1ead62654c0d4d12f6282947669e53f603326
Akul Srivastava <akulsr0@gmail.com>
2022-05-29 20:48:09 +0530

    add deps

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ import "./styles.css";
 
 export default function App() {
   const [persons, setPersons] = useState([]);
+  const [n, setN] = useState(5);
 
   useTheEffect(() => {
-    fetch("https://randomuser.me/api?results=10")
+    fetch(`https://randomuser.me/api?results=${n}`)
       .then((res) => res.json())
       .then(({ results }) => {
         console.log(results);
         setPersons(results);
       });
-  }, []);
+  }, [n]);
 
   return (
     <div className="App">
+      <h1>{n}</h1>
+      <button onClick={() => setN(n - 1)}>-</button>
+      <button onClick={() => setN(n + 1)}>+</button>
       {persons.map((p) => (
         <h3 key={p.email}>{p.name.first}</h3>
       ))}
